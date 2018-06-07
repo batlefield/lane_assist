78380684dba032e896e5b1166f71417ea9fc9337
Peter <peter.maticic@gmail.com>
2018-06-07 17:40:28 +0200

    Izirsovanje crt narejeno

--- main.cpp
+++ main.cpp
@@ -14,6 +14,8 @@ Point2f upperRight(376, 236);
 Point2f lowerRight(633, 348);
 Point2f lowerLeft(67, 353);
 
+Point2i prevLanePoints[8];
+
 //processes captured image
 Mat process(Mat frame) {
     Mat transformed, colorMask, sobelMask, finalMask;
@@ -22,9 +24,11 @@ Mat process(Mat frame) {
 
     //warpamo perspektivo da dobimo cesto na sliki - tako izracunamo radij crt
     Point2f inputQuad[] = {upperLeft, upperRight, lowerRight, lowerLeft};
-    Point2f outputQuad[] = {Point2f(0, 0), Point2f(frameSize.width, 0), Point2f(frameSize.width, frameSize.height), Point2f(0, frameSize.height)};
+    Point2f outputQuad[] = {Point2f(0, 0), Point2f(frameSize.width, 0), Point2f(frameSize.width, frameSize.height),
+                            Point2f(0, frameSize.height)};
 
     Mat transMatrix = getPerspectiveTransform(inputQuad, outputQuad);
+
 
     warpPerspective(frame, transformed, transMatrix, frameSize);
 
@@ -49,49 +53,76 @@ Mat process(Mat frame) {
 
     Point2i lanePoints[8];
 
+    //sestejemo bele piksle blocno po stolpcih
     int summed[8][width];
-    for(int i = 0; i < 8; i++) {
-        for(int j = 0; j < width; j++) {
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < width; j++) {
             Mat sub(finalMask, Rect(j, i * (height / 8), 1, height / 8));
             summed[i][j] = countNonZero(sub);
         }
     }
 
-
-    for(int i = 0; i < 8; i++) {
+    //najdemo levi in desni maksimum
+    for (int i = 0; i < 8; i++) {
         float maxl = 0, maxr = 0;
         int maxli = 0, maxri = width - 1;
-        for(int j = 0; j < width / 2; j++) {
+        for (int j = 0; j < width / 2; j++) {
             float l = summed[i][j];
             float r = summed[i][width - j - 1];
-            if(l > maxl) {
+            if (l > maxl) {
                 maxl = l;
                 maxli = j;
             }
-            if(r > maxr) {
+            if (r > maxr) {
                 maxr = r;
                 maxri = width - j;
             }
         }
+        if (maxri < width / 2) maxri = width - 1;
+
+        /*if(i > 0) {
+            if(maxli == 0) {
+                maxli = prevLanePoints[i - 1].x;
+            }
+
+            if(maxri == width - 1) {
+                maxri = prevLanePoints[i - 1].y;
+            }
+        }*/
         lanePoints[i] = Point2i(maxli, maxri);
         //printf("%d, %d\n", maxli, maxri);
     }
 
+    for (int i = 0; i < 8; i++) {
+        prevLanePoints[i] = lanePoints[1];
+    }
 
     cvtColor(finalMask, finalMask, CV_GRAY2BGR);
 
-    for(int i = 0; i < 8; i++) {
+    for (int i = 0; i < 8; i++) {
         int ch = (i * (height / 8)) + (height / 16);
         circle(finalMask, Point2i(lanePoints[i].x, ch), 4, Scalar(0, 0, 255));
         circle(finalMask, Point2i(lanePoints[i].y, ch), 4, Scalar(0, 0, 255));
     }
 
-    return finalMask;
+    Mat result = Mat::zeros(finalMask.size(), frame.type());
+
+    //narisemo crte
+    for(int i = 1; i < 8; i++) {
+        if(lanePoints[i - 1].x != 0 && lanePoints[i].x != 0) line(result, Point(lanePoints[i - 1].x, (i - 1) * (height / 8) + (height / 16)), Point(lanePoints[i].x, i * (height / 8) + (height / 16)), Scalar(255, 0, 0, 0), 20);
+        if(lanePoints[i - 1].y != width - 1 && lanePoints[i].y != width - 1) line(result, Point(lanePoints[i - 1].y, (i - 1) * (height / 8) + (height / 16)), Point(lanePoints[i].y, i * (height / 8) + (height / 16)), Scalar(255, 0, 0, 0), 20);
+    }
+
+    Mat warpedBack;
+    warpPerspective(result, warpedBack, transMatrix, frameSize, WARP_INVERSE_MAP, BORDER_TRANSPARENT);
+
+
+    return frame + warpedBack;
 }
 
 int main(int argc, char **argv) {
     VideoCapture camera;
-    if(argc > 1) {
+    if (argc > 1) {
         //posnetek
         camera = VideoCapture(argv[1]);
     } else {
@@ -102,7 +133,7 @@ int main(int argc, char **argv) {
     Mat frame;
 
 
-    while(true) {
+    while (true) {
         camera.read(frame);
         Mat detect;
         frame.copyTo(detect);
@@ -110,8 +141,8 @@ int main(int argc, char **argv) {
         imshow("Camera", frame);
         imshow("Processed", processed);
 
-        int c = waitKey(-1);
-        if(c == (int)'q') {
+        int c = waitKey(44);
+        if (c == (int) 'q') {
             break;
         }
 
